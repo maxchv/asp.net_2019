241f0df2cdef994160df3d8718eeb135977a9d64
Maxim Shaptala <maxshaptala@gmail.com>
2019-04-11 12:17:58 +0300

    Добавлен пример работы со свойством Server, пример загрузки файлов на сервер

---
+++ lessons/Lesson03/UploadFilesDemo/Default.aspx.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+
+namespace UploadFilesDemo
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        private readonly string _uploads;
+        private readonly string _uploadsPath;
+
+        public Default()
+        {
+            _uploads = "Uploads";
+            _uploadsPath = Server.MapPath(_uploads);
+        }
+        
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            BindFileList();
+        }
+
+        private void BindFileList()
+        {
+            var links = Directory.GetFiles(_uploadsPath)
+                .Select(s => new FileInfo(s).Name)
+                .Select(f => new {Text = f, Url = $"./{_uploads}/{f}"});
+
+            BulletedList1.DataSource = links;
+            BulletedList1.DataTextField = "Text";
+            BulletedList1.DataValueField = "Url";
+            BulletedList1.DataBind();
+        }
+
+        protected void Upload(object sender, EventArgs e)
+        {
+            if (!FileUpload1.HasFile) return;
+
+            FileUpload1.SaveAs(Path.Combine(_uploadsPath, FileUpload1.FileName));
+            Response.Redirect("Default.aspx");
+        }
+    }
+}
---
+++ lessons/Lesson03/UploadFilesDemo/ServerDemo.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="ServerDemo.aspx.cs" Inherits="UploadFilesDemo.ServerDemo" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <h1>Hello World!</h1>
+        <div>
+            <asp:Label ID="PathToDir" runat="server" Text="Label"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
