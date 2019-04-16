e7312082b89c41f591289b7e3e1442cb27e566a9
Maxim Shaptala <maxshaptala@gmail.com>
2019-04-16 12:22:44 +0300

    Добавлен первый урок по MVC

---
+++ lessons/lesson05/HelloMVC/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HelloMVC.Controllers
+{
+    public class HomeController: Controller
+    {
+        public ActionResult Index(int? id)
+        {
+            ViewBag.Title = "Title from controller";
+            ViewData["message"] = "Hello friend";
+            TempData["flash"] = "Flash data";
+            return View(id);
+        }
+    }
+}
