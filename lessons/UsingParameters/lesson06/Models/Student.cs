635eedeba7934ce27780d1010f01185817913355
Maxim Shaptala <maxshaptala@gmail.com>
2019-04-18 11:05:27 +0300

    Добавлен пример работы с формой, строго типизированным представлением, шаблоном страниц

---
+++ lessons/UsingParameters/lesson06/Models/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace lesson06.Models
+{
+    public class Student
+    {
+        private static int count;
+        public Student()
+        {
+            Id = ++count;
+        }
+        public int Id { get; private set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
