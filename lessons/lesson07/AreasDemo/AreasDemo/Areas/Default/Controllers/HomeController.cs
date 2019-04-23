3c85d3ea75ae1790080bd5e00facd79ea7d6696d
Maxim Shaptala <maxshaptala@gmail.com>
2019-04-23 10:42:38 +0300

    Добавлен пример работы с layout

---
+++ lessons/lesson07/AreasDemo/AreasDemo/Areas/Default/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace AreasDemo.Areas.Default.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ lessons/lesson07/AreasDemo/AreasDemo/Areas/Default/DefaultAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace AreasDemo.Areas.Default
+{
+    public class DefaultAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Default";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Default_default",
+                "Default/{controller}/{action}/{id}",
+                new { action = "Index", controller="Home", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
