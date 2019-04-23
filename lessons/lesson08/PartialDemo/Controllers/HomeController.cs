20584055fb02f1b318adb179f108eb09e0d42eaa
Maxim Shaptala <maxshaptala@gmail.com>
2019-04-23 11:53:07 +0300

    Добавлен пример работы с частичными представлениями, видео по работе с областями

---
+++ lessons/lesson08/PartialDemo/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using PartialDemo.Models;
+
+namespace PartialDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult PartialResult(int? id)
+        {
+            var cars = CarRepository.Instance.Cars();
+            return PartialView("_Car", cars[id ?? 0]);
+        }
+
+        public ActionResult AllCars()
+        {
+            var cars =  CarRepository.Instance.Cars();
+            return PartialView("_Cars", cars);
+        }
+
+        [ChildActionOnly] // <-- вызов только во view
+        public string CurrentDate()
+        {
+            return DateTime.Now.ToLongDateString();
+        }
+    }
+}
---
+++ lessons/lesson08/PartialDemo/Models/CarRepository.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace PartialDemo.Models
+{
+    public class CarRepository
+    {
+        private readonly List<Car> _cars = new List<Car>
+        {
+            new Car{Id=1, Name = "Audi A8", Color = "Red", Price = 25_000.0m},
+            new Car{Id=2, Name = "BMW X9", Color = "Black", Price = 15_000.0m},
+            new Car{Id=3, Name = "Porche", Color = "Yellow", Price = 35_000.0m},
+            new Car{Id=4, Name = "Таврия", Color = "Magenta", Price = 123_000.0m},
+        };
+        private static CarRepository instance = new CarRepository();
+        public static CarRepository Instance => instance;
+
+        private CarRepository()
+        {
+            
+        }
+
+        public List<Car> Cars(int delay = 3000)
+        {
+            Thread.Sleep(delay);
+            return _cars;
+        }
+    }
+}
