55b780fe4e987bedc75989d5131349bc9c0c03ec
Maxim Shaptala <maxshaptala@gmail.com>
2019-05-16 09:54:59 +0300

    Стартовый ASP.NET MVC проект с включенным Identity Framework

---
+++ lessons/lesson13/lesson13/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(lesson13.Startup))]
+namespace lesson13
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
