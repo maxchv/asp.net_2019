25d1db1e95a0c4c4645d68b55d5c0d9fef40a93f
Maxim Shaptala <maxshaptala@gmail.com>
2019-05-27 10:10:14 +0300

    Добавлен файл настроек appsettigns.json и продемонстрирована работа с переменными окружения

--- lessons/lesson17/lesson17/Startup.cs
+++ lessons/lesson17/lesson17/Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace lesson17
@@ -15,10 +16,11 @@ namespace lesson17
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IConfiguration config)
         {
             if (env.IsDevelopment())
             {
@@ -27,7 +29,20 @@ namespace lesson17
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                context.Response.ContentType = "text/html";
+                Func<String> test = () =>
+                {
+                    if (true) throw new Exception();
+                    return "test";
+                }; 
+                await context.Response.WriteAsync($"<h1>{config["AppName"]}</h1>" +
+                                                  //$"{test()}" +
+                                                  $"<p>Env: {env.EnvironmentName}</p>" +
+                                                  $"<p>ApplicationName: {env.ApplicationName}</p>" +
+                                                  $"<p>WebRootPat: {env.WebRootPath}</p>" +
+                                                  $"<p>ContentRootPath: {env.ContentRootPath}</p>" +
+                                                  $"<p>Default connection: {config.GetConnectionString("DefaultConnection")}</p>" +
+                                                  $"<p>MySql connection: {config.GetSection("ConnectionStrings")["MySqlConnection"]}");
             });
         }
     }
