447a1e90fabed060ed4e3f03dfd5b687eab8b64c
Maxim Shaptala <maxshaptala@gmail.com>
2019-05-28 10:11:11 +0300

    Добавлены модели студентов и предметов

---
+++ lessons/lesson18/lesson18/Models/StudentModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace lesson18.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public virtual IEnumerable<SubjectMarks> SubjectsMarks { get; set; }
+    }
+
+    public class SubjectMarks
+    {
+        public int Id { get; set; }
+        public int SubjectId { get; set; }
+        public int StudentId { get; set; }
+        [DisplayName("Оценка")]
+        public int Mark { get; set; }
+    }
+
+    public class Subject
+    {
+        public int Id { get; set; }
+        [DisplayName("Название предмета")]
+        public string Name { get; set; }
+
+        public virtual IEnumerable<SubjectMarks> SubjectMarks { get; set; }
+    }
+}
