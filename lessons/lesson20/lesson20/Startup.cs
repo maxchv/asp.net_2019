761a853e59544530941b968db9b78a1b491131da
Maxim Shaptala <maxshaptala@gmail.com>
2019-05-30 10:44:18 +0300

    реализован middleware для обработки запроса по протоколу WebSocket

--- lessons/lesson20/lesson20/Startup.cs
+++ lessons/lesson20/lesson20/Startup.cs
@@ -1,9 +1,11 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore.Query.ExpressionVisitors.Internal;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -38,6 +40,39 @@ namespace lesson20
             }
 
             app.UseStaticFiles();
+
+            app.UseWebSockets(options: new WebSocketOptions
+            {
+                ReceiveBufferSize = 4 * 1024
+            });
+
+            app.Use(async (context, next) =>
+            {
+                var path = context.Request.Path;
+                if (path == "/ws" && context.WebSockets.IsWebSocketRequest)
+                {
+                    // запрос на установку соедиенения по WebSocket
+                    var webSocket = await context.WebSockets.AcceptWebSocketAsync();
+                    var buffer = new byte[1024 * 4]; // буфер
+                    // получаем первое сообщение
+                    var receive = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+                    while (!receive.CloseStatus.HasValue) // до тех пор, пока открыто соединение
+                    {
+                        // отправляем ответ
+                        await webSocket.SendAsync(new ArraySegment<byte>(buffer), receive.MessageType, 
+                                                  receive.EndOfMessage, CancellationToken.None);
+                        // получаем очередное сообщение
+                        receive = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+                    }
+                    // закрываем соединение
+                    await webSocket.CloseAsync(receive.CloseStatus.Value, receive.CloseStatusDescription,
+                        CancellationToken.None);
+                }
+                else
+                {
+                    await next();
+                }
+            });
 
             app.UseMvc();
         }
